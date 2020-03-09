98b1eae29426755eeee89c519296b43d037c32d0
Frank Iannelli <frank.iannelli@outlook.com>
2020-03-10 10:55:07 +1100

    add readme

---
+++ ec2-setup-server.sh
@@ -0,0 +1,15 @@
+sudo yum update
+sudo yum install git -y
+sudo yum install docker -y
+sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
+npm install yarn -g
+npm install pm2@latest -g
+pm2 start ~/resources/analyticsDb/src/getDbBackupData.js --cron "* */24 * * *"
